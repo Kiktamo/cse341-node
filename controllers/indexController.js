d8743b042a21d8cffd31fe463e98f21c41c6ded2
Kiktamo <mccrary@gmx.us>
2023-05-06 17:38:51 -0400

    W3 Personal Assignment

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -5,9 +5,40 @@ index = (req, res) => {
   res.send("Mickey McCrary");
 };
 
+const getAll = async (req, res) => {
+  const db = mongodb.getDb();
+  const collection = db.db('cse341').collection('contacts');
+  const documents = await collection.find().toArray();
+  res.send(documents);
+};
+
+const getById = async (req, res) => {
+  const db = mongodb.getDb();
+  const collection = db.db('cse341').collection('contacts');
+
+  const id = String(req.params.id);
+  const contact = await collection.findOne({
+    _id: new mongo.ObjectId(id)
+  });
+
+  if (!contact) {
+    res.status(404).send('Contact not found');
+  } else {
+    res.send(contact);
+  }
+};
+
 const insert = async (req, res) => {
   const db = mongodb.getDb();
   const collection = db.db('cse341').collection('contacts');
+
+  const requiredFields = ['firstName', 'lastName', 'email', 'favoriteColor', 'birthday'];
+  const missingFields = requiredFields.filter(field => !req.body[field]);
+
+  if (missingFields.length > 0) {
+    res.status(400).send(`Missing fields: ${missingFields.join(', ')}`);
+    return;
+  }
 
   const {
     firstName,
@@ -29,34 +60,77 @@ const insert = async (req, res) => {
     console.log(err);
     res.status(500).send('Error inserting document');
   } else {
-    res.send('Document inserted');
+    res.status(201).send('Document inserted. ID = ' + result.insertedId);
   }
 };
 
-const getAll = async (req, res) => {
-  const db = mongodb.getDb();
-  const collection = db.db('cse341').collection('contacts');
-  const documents = await collection.find().toArray();
-  res.send(documents);
-};
-
-const getById = async (req, res) => {
+const updateById = async (req, res) => {
   const db = mongodb.getDb();
   const collection = db.db('cse341').collection('contacts');
 
   const id = String(req.params.id);
-  const contact = await collection.findOne({ _id: new mongo.ObjectId(id)});
+  const contact = await collection.findOne({
+    _id: new mongo.ObjectId(id)
+  });
 
   if (!contact) {
     res.status(404).send('Contact not found');
+    return;
+  }
+
+  const fieldsToUpdate = Object.keys(req.body);
+  const validFields = ['firstName', 'lastName', 'email', 'favoriteColor', 'birthday'];
+
+  const canUpdate = fieldsToUpdate.every(field => validFields.includes(field));
+  if (!canUpdate) {
+    const invalidFields = fieldsToUpdate.filter(field => !validFields.includes(field));
+    res.status(400).send(`Invalid fields: ${invalidFields.join(', ')}`);
+    return;
+  }
+
+  const updateValues = {};
+  fieldsToUpdate.forEach(field => {
+    updateValues[field] = req.body[field];
+  });
+
+  const result = await collection.updateOne({
+    _id: new mongo.ObjectId(id)
+  }, {
+    $set: updateValues
+  });
+
+  if (!result.acknowledged) {
+    console.log(err);
+    res.status(500).send('Error updating document');
   } else {
-    res.send(contact);
+    res.status(204).send('Document Updated');
   }
 };
+
+const deleteById = async (req, res) => {
+  const db = mongodb.getDb();
+  const collection = db.db('cse341').collection('contacts');
+
+  const id = String(req.params.id);
+
+  const result = await collection.deleteOne({
+    _id: new mongo.ObjectId(id)
+  });
+
+  if (result.deletedCount === 0) {
+    res.status(404).send('Contact not found');
+  } else {
+    res.status(200).send('Contact deleted');
+  }
+};
+
+
 
 module.exports = {
   index,
   insert,
   getAll,
-  getById
+  getById,
+  updateById,
+  deleteById
 }
--- routes/contacts.js
+++ routes/contacts.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const indexController = require('../controllers/indexController');
 
-router.post('/insert', indexController.insert)
 router.get('/contacts', indexController.getAll)
 router.get('/contacts/:id', indexController.getById);
+router.post('/contacts', indexController.insert)
+router.put('/contacts/:id', indexController.updateById)
+router.delete('/contacts/:id', indexController.deleteById)
 
 module.exports = router;
