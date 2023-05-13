8baab206520af07a3f9f931d2e2546d6c07af894
Kiktamo <mccrary@gmx.us>
2023-05-13 16:47:08 -0400

    Typo

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./db/connect');
 
 app.use(bodyParser.json())
-.use(function(req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
      next();
-});e('/', indexRouter);
+});
+app.use('/', indexRouter);
 
 const port = config.port;
 
